ba82003f9f81cb23733b4f871159290d8f2a6ce3
kpalanca <kpalanca@canyonranch.com>
2026-01-26 20:12:20 -0600

    Initial Commit

---
+++ gripstrength/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gripstrength
+//
+//  Created by Katrina Palanca on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gripstrength/gripstrengthApp.swift
@@ -0,0 +1,17 @@
+//
+//  gripstrengthApp.swift
+//  gripstrength
+//
+//  Created by Katrina Palanca on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct gripstrengthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
